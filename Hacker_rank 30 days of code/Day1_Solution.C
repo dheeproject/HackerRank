b83494f7e239f5443f146b61c4cc1c0d10e43271
Dheeraj Mansukhani <62594025+dheeproject@users.noreply.github.com>
2020-08-23 21:27:04 +0530

    Create Day1_Solution.C
    .C

---
+++ Hacker_rank 30 days of code/Day1_Solution.C
@@ -0,0 +1,35 @@
+For problem Description,Check out the link:
+shorturl.at/opsyW
+****************************************Solution****************************************************
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int i = 4;
+    double d = 4.0;
+    char s[] = "HackerRank ";
+int a;
+double b;
+char c[100];
+char temp;
+    // Declare second integer, double, and String variables.
+    scanf("%d",&a);
+    scanf("%lf",&b);
+    scanf("%c",&temp);
+    scanf("%[^\n]",c);
+        // Read and save an integer, double, and String to your variables.
+    printf("%d",a+i);
+    printf("\n%.1f",d+b);
+    printf("\n");
+    printf("%s",s);
+    printf("%s",c);
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+return 0;
+}
